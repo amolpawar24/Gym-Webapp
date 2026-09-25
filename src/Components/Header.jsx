9ca3bfd4cde9b191314c3756a25cf70f762f99d4
amolpawar24 <amolpawar.me@gmail.com>
2026-09-25 17:34:09 +0530

    Add Header component for main page layout

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div id='main'>
+    <div id='name'>
+        <div className='header-headings'>
+            <h2>STEP UP YOUR</h2>
+            <h1><span>FITNESS</span> WITH US</h1>
+            <p className="details">Build Your Body And Fitness With Professionals Touch</p>
+
+            {/* <div className="header-btn">JOIN US</div> */}
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
